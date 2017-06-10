9e7972220fa3e3da6de49f5f35e6a893285f7615
jnanda <jyoti.kaustuv.nanda@gmail.com>
2017-06-09 18:39:32 -0700

    adding jsdoc to the project

--- src/js/components/Header/hamburger.js
+++ src/js/components/Header/hamburger.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+/**
+ * Represents Hamburger or Menu.
+ * @function
+ * @param {object} props - props passed from parent component
+ */
 const Hamburger = props => {
   return (
     <div className={`menu ${(!props.open)? 'close' : ''}`} onClick={props.onClick}>
